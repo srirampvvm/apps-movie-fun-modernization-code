d4c7c99b16bbe81e53e161f96f1c53e10d7e965b
srirampvvm <plnvbs@gmail.com>
2018-04-25 14:59:02 -0500

    circuit-breaker-hystrix

--- applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
+++ applications/movie-fun-app/src/main/java/org/superbiz/moviefun/Application.java
@@ -8,13 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.superbiz.moviefun.albumsapi.CoverCatalog;
 import org.superbiz.moviefun.blobstore.BlobStore;
 import org.superbiz.moviefun.blobstore.S3Store;
 import org.superbiz.moviefun.moviesapi.MovieServlet;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
 public class Application {
 
     public static void main(String... args) {
@@ -29,6 +32,11 @@ public class Application {
     @Bean
     ServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapServices) {
         return new ServiceCredentials(vcapServices);
+    }
+
+    @Bean
+    public CoverCatalog coverCatalog(BlobStore blobStore) {
+        return new CoverCatalog(blobStore);
     }
 
     @Bean
